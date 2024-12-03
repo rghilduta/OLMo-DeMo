bfb851309b1d856cbb72fef42d81642547c4c497
Robert Ghilduta <robert.ghilduta@nuand.com>
2024-12-03 02:14:32 +0000

    don't feel like dealing with wandb config

--- scripts/train.py
+++ scripts/train.py
@@ -105,6 +105,7 @@ def main(cfg: TrainConfig) -> None:
     if cfg.wandb is not None and (get_global_rank() == 0 or not cfg.wandb.rank_zero_only):
         wandb_dir = Path(cfg.save_folder) / "wandb"
         wandb_dir.mkdir(parents=True, exist_ok=True)
+        cfg.wandb.entity="rgbrxg"
         wandb.init(
             dir=wandb_dir,
             project=cfg.wandb.project,
